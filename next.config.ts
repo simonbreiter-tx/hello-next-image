111fe95853f47a398bbed5535d70b110aa7707da
Simon Breiter <hello@simonbreiter.com>
2024-12-13 10:15:38 +0100

    feat: add simple test image

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'place.simonbreiter.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    deviceSizes: [240, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
 };
 
 export default nextConfig;
